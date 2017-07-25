77fdd004829ec16fc79ad25443bee7c9319233c6
R3Sean <30439474+R3Sean@users.noreply.github.com>
2017-07-25 14:30:40 -0400

    Add files via upload

---
+++ interviews-master/dot-net/UnitTesting/WriteUnitTest.UnitTests/Services/LessonServiceUnitTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace WriteUnitTest.UnitTests.Services
+{
+    [TestClass]
+    public class LessonServiceUnitTests
+    {
+        [TestMethod]
+        public void UpdateLessonGradeFailTest()
+        {
+            var lessonSVC = new LessonService();
+            Assert.IsTrue(!lessonSVC.UpdateLessonGrade(12, 79));
+        }
+
+		[TestMethod]
+		public void UpdateLessonGradePassTest()
+		{
+			var lessonSVC = new LessonService();
+			Assert.IsTrue(lessonSVC.UpdateLessonGrade(12, 85));
+		}
+
+    }
+}
---
+++ interviews-master/dot-net/UnitTesting/WriteUnitTest/Program.cs
@@ -0,0 +1,20 @@
+﻿using WriteUnitTest.Services;
+using System;
+
+namespace WriteUnitTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var lessonSvc = new LessonService();
+
+            var lessonId = 12;
+
+            var grade = 98.2d;
+
+            lessonSvc.UpdateLessonGrade(lessonId, grade);
+
+        }
+    }
+}
